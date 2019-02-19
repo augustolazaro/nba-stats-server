26c935012f297d2f07cc85a9b478caa5f9933597
augustolazaro <aemlazaro@gmail.com>
2019-02-18 23:27:10 -0300

    Production

--- src/server.ts
+++ src/server.ts
@@ -2,20 +2,21 @@ import express from 'express'
 import express_graphql from 'express-graphql'
 import cors from 'cors'
 
-import nba from 'nba'
-
 import { schema } from './schema.js';
 
-const PORT = 4000
+const PORT = process.env.PORT || 4000
 
 let app = express();
 app.use(cors())
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 app.use('/graphql', express_graphql({
     schema,
     graphiql: true,
 }));
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-// nba.stats.teamInfoCommon({ TeamID: 1610612746 })
-//     .then(res => console.log('--- res:', res))
-//     .catch(err => console.log('--- err:', err))
