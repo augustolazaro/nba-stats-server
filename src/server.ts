9f868cef63399cd52c2da6042e4dceedb33e8882
augustolazaro <aemlazaro@gmail.com>
2019-02-19 00:06:17 -0300

    Fix dependencies

--- src/server.ts
+++ src/server.ts
@@ -8,13 +8,6 @@ const PORT = process.env.PORT || 4000
 
 let app = express();
 app.use(cors())
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
 app.use('/graphql', express_graphql({
     schema,
     graphiql: true,
